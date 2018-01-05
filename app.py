42204582ab657f3ff95e17dbbd218e4c87fe4f58
Roy Hanley <royhanley@Roys-MBP.home>
2018-01-05 17:44:27 +0000

    Added success and fail pages

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ def home():
 @app.route('/space-invaders')
 def space_invaders():
     return render_template('space_invaders.html')
+
+
+@app.route('/player-fail')
+def player_fail():
+    return render_template('player_fail.html')
+
+
+@app.route('/player-success')
+def player_success():
+    return render_template('player_success.html')
+
+
+@app.route('/download-cv')
+def download_cv():
+    # download CV
+    return
